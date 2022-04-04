32c85646955fe4ffc337ec252e4b230c81c98ea4
yakovbenalex <yakovbenalex@mail.ru>
2022-04-04 20:40:02 +0300

    Cleanup code

--- app/src/main/java/ru/opalevapps/andersenandroidlesson6recyclerviewcontactlist/ContactRecyclerAdapter.kt
+++ app/src/main/java/ru/opalevapps/andersenandroidlesson6recyclerviewcontactlist/ContactRecyclerAdapter.kt
@@ -15,9 +15,6 @@ class ContactRecyclerAdapter(
     private val itemClickListener: OnItemClickListener
 
 ) : RecyclerView.Adapter<ContactRecyclerAdapter.MyViewHolder>() {
-//    private lateinit var firstName: TextView
-//    private lateinit var lastName: TextView
-//    private lateinit var phone: TextView
 
     class MyViewHolder(itemView: View) :
         RecyclerView.ViewHolder(itemView) {
@@ -38,45 +35,16 @@ class ContactRecyclerAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val itemView = LayoutInflater.from(context).inflate(R.layout.item_contact, parent, false)
-
-//        firstName = itemView!!.findViewById(R.id.tvContactFirstName)
-//        lastName = itemView.findViewById(R.id.tvContactLastName)
-//        phone = itemView.findViewById(R.id.tvContactPhone)
-
-//        firstName.text = contacts[p0].firstName
-//        lastName.text = contacts[p0].lastName
-//        phone.text = contacts[p0].phone
         return MyViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-//        val contact = contactsArrayList[position]
         holder.bind(contactsArrayList[position], itemClickListener, position)
-//        holder.firstName.text = contact.firstName
-//        holder.lastName.text = contact.lastName
-//        holder.phone.text = contact.phone
     }
 
     override fun getItemCount(): Int = contactsArrayList.size
 
     override fun getItemId(p0: Int): Long = p0.toLong()
-
-//    override fun getCount(): Int = contacts.size
-//
-//    override fun getItem(p0: Int): Any = contacts[p0]
-
-
-    /*override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
-        val view = LayoutInflater.from(context).inflate(R.layout.item_contact, p2, false)
-
-        firstName = view!!.findViewById(R.id.tvContactFirstName)
-        lastName = view.findViewById(R.id.tvContactLastName)
-        phone = view.findViewById(R.id.tvContactPhone)
-        firstName.text = contacts[p0].firstName
-        lastName.text = contacts[p0].lastName
-        phone.text = contacts[p0].phone
-        return view
-    }*/
 }
 
 interface OnItemClickListener{
